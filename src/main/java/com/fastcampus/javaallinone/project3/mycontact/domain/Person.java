3b03f45e48922fab84ff9eac71fbab78be8bcdcf
양기현 <rlgusdid@naver.com>
2021-06-03 21:20:05 +0900

    Chapter 03. JPA - 03. Lombok (2) - 1

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -1,14 +1,15 @@
 package com.fastcampus.javaallinone.project3.mycontact.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 @ToString
@@ -19,8 +20,10 @@ public class Person {
     @Id
     private Long id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private int age;
 
     private String hobby;
@@ -36,4 +39,17 @@ public class Person {
     @ToString.Exclude
     private String phoneNumber;
 
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        Person person = (Person) object;
+
+        if (!person.getName().equals(this.getName())) {
+            return false;
+        }
+
+        return person.getAge() == this.getAge();
+    }
 }
--- src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
+++ src/test/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -33,5 +34,14 @@ class PersonRepositoryTest {
         assertThat(peoples.get(0).getBloodType()).isEqualTo("A");
     }
 
+    @Test
+    void hashCodeAndEquals() {
+        Person person1 = new Person("martin", 10);
+        Person person2 = new Person("martin", 10);
+
+        System.out.println(person1.equals(person2));
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+    }
 
 }
