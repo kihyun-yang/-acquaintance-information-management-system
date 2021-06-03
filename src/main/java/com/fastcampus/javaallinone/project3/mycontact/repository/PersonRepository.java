dfea0a9ddbf68ffc2745ce478fabf5ed2c9c418f
양기현 <rlgusdid@naver.com>
2021-06-03 23:10:11 +0900

    Chapter04.04. JPA Query Method

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepository.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepository.java
@@ -2,7 +2,17 @@ package com.fastcampus.javaallinone.project3.mycontact.repository;
 
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    List<Person> findByName(String name);
+
+    List<Person> findByBlockIsNull();
+
+    List<Person> findByBloodType(String bloodType);
+
+    List<Person> findByBirthdayBetween(LocalDate startDate, LocalDate endDate);
 }
